a34e5800db388661c1f695d37c72ff7266665522
nuuuwan <nuuuwan@gmail.com>
2023-02-25 17:04:27 +0530

    data: sltda (200)

--- examples/sltda.py
+++ examples/sltda.py
@@ -4,6 +4,6 @@ if __name__ == '__main__':
     spider = Spider(
         'https://www.sltda.gov.lk/en/monthly-tourist-arrivals-reports-2023'
     )
-    table_paths = spider.spider_tables(limit=100)
+    table_paths = spider.spider_tables(limit=200)
     for table_path in table_paths:
         print(table_path)
