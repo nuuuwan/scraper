5d2991cfc06ce7c5bcbccc4c21dabbf3079022ef
nuuuwan <nuuuwan@gmail.com>
2023-02-25 07:55:13 +0530

    tests: various

--- tests/test_link_scraper.py
+++ tests/test_link_scraper.py
@@ -1,12 +1,11 @@
-from unittest import TestCase, skip
+from unittest import TestCase
 
 from scraper import LinkScraper
-from tests.test_base_scraper import TEST_URL
+from tests.test_base_scraper import TEST_URL, TEST_URL_PDF
 
 
 class TestLinkScraper(TestCase):
-    @skip('content changes too often')
     def test_urls(self):
         urls = LinkScraper(TEST_URL).urls
-        self.assertEqual(len(urls), 44)
-        self.assertEqual(urls[0], '#')
+        self.assertEqual(len(urls), 1)
+        self.assertEqual(urls[0], TEST_URL_PDF)
--- tests/test_pdf_scraper.py
+++ tests/test_pdf_scraper.py
@@ -1,8 +1,7 @@
-import os
 from unittest import TestCase
 
 from scraper import PDFScraper
-from tests.test_base_scraper import TEST_URL
+from tests.test_base_scraper import TEST_URL, TEST_URL_PDF
 
 
 class TestPDFScraper(TestCase):
@@ -10,10 +9,7 @@ class TestPDFScraper(TestCase):
         scraper = PDFScraper(TEST_URL)
         self.assertEqual(
             scraper.pdf_urls[0],
-            os.path.join(
-                'https://www.sltda.gov.lk/storage/common_media',
-                'Tourism_Growth_Trends_20211292502899.pdf',
-            ),
+            TEST_URL_PDF,
         )
 
     def test_download_all(self):
