20ecc2ab0812857428577f495fb247cce6cb2205
nuuuwan <nuuuwan@gmail.com>
2025-09-15 15:54:21 +0530

    test_readme

--- tests/pdf_scraper/readme/test_readme.py
+++ tests/pdf_scraper/readme/test_readme.py
@@ -1,8 +1,20 @@
 import os
 import unittest
+from functools import cached_property
 from unittest.mock import patch
 
 from pdf_scraper import AbstractDoc, AbstractHomePage, ReadMe
+
+
+class DummyHomePage(AbstractHomePage):
+    def __init__(self):
+        super().__init__("http://example.com")
+
+
+class DummyDoc(AbstractDoc):
+    @cached_property
+    def remote_data_url(self) -> str:
+        return "http://example.com/data.json"
 
 
 class TestCase(unittest.TestCase):
@@ -10,15 +22,16 @@ class TestCase(unittest.TestCase):
         readme = ReadMe(
             home_page_class=AbstractHomePage, doc_class=AbstractDoc
         )
+        self.assertEqual(len(readme.doc_list), 0)
         readme.build()
 
     def test_full(self):
         mock_dir_root = os.path.join("tests", "output", "data_parent")
         with patch.object(
-            AbstractDoc,
+            DummyDoc,
             "list_all",
             return_value=[
-                AbstractDoc(
+                DummyDoc(
                     num="1234567890",
                     date_str="2023-10-01",
                     description="Test document",
@@ -26,11 +39,7 @@ class TestCase(unittest.TestCase):
                     url_metadata="http://example.com/metadata.json",
                 )
             ],
-        ), patch.object(
-            AbstractDoc, "get_dir_root", return_value=mock_dir_root
-        ):
-            readme = ReadMe(
-                home_page_class=AbstractHomePage, doc_class=AbstractDoc
-            )
+        ), patch.object(DummyDoc, "get_dir_root", return_value=mock_dir_root):
+            readme = ReadMe(home_page_class=DummyHomePage, doc_class=DummyDoc)
             self.assertEqual(len(readme.doc_list), 1)
             readme.build()
