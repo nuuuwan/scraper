78b526e724e624efb81dcb2baa078edbdde59120
nuuuwan <nuuuwan@gmail.com>
2025-09-16 12:04:59 +0530

    Added tests

--- tests/utils_future/test_big_json_file.py
+++ tests/utils_future/test_big_json_file.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import unittest
 from unittest.mock import patch
 
@@ -19,3 +20,14 @@ class TestCase(unittest.TestCase):
             big_json_file.write(data_list)
             data_list2 = big_json_file.read()
             self.assertEqual(data_list, data_list2)
+
+    def test_empty(self):
+        dir_path = os.path.join("tests", "output", "test_big_json_file_empty")
+        shutil.rmtree(dir_path, ignore_errors=True)
+        big_json_file = BigJSONFile(dir_path)
+        data_list = big_json_file.read()
+        self.assertEqual(data_list, [])
+        big_json_file.write([])
+        data_list2 = big_json_file.read()
+        self.assertEqual(data_list2, [])
+        shutil.rmtree(dir_path, ignore_errors=True)
