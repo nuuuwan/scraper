77026f4a93bf614a670afa6479fdc0ae3f2e6999
nuuuwan <nuuuwan@gmail.com>
2025-09-18 11:53:16 +0530

    -> time_str

--- src/utils_future/Parse.py
+++ src/utils_future/Parse.py
@@ -5,7 +5,6 @@ class Parse:
     TIME_FORMAT = "%Y-%m-%d %H:%M"
 
     @staticmethod
-    def time(x) -> str:
-
+    def time_str(x) -> str:
         dt = parser.parse(x)
         return dt.strftime(Parse.TIME_FORMAT)
