2a1997d574c4e6318e530efdfa11cfd0627fb48b
nuuuwan <nuuuwan@gmail.com>
2025-09-18 11:01:15 +0530

    Added Parse

---
+++ src/utils_future/Parse.py
@@ -0,0 +1,11 @@
+from dateutil import parser
+
+
+class Parse:
+    TIME_FORMAT = "%Y-%m-%d %H:%M"
+
+    @staticmethod
+    def time(x) -> str:
+
+        dt = parser.parse(x)
+        return dt.strftime(Parse.TIME_FORMAT)
