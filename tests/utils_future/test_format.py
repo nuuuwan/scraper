946caa8ada7783fb6004af7896715627580b8315
nuuuwan <nuuuwan@gmail.com>
2025-09-19 11:00:47 +0530

    Added test_format

---
+++ tests/utils_future/test_format.py
@@ -0,0 +1,33 @@
+import unittest
+
+from utils_future.Format import Format
+
+
+class TestCase(unittest.TestCase):
+    def test_badge(self):
+        for x, expected_result in [
+            ("hello-world", "hello--world"),
+            ("hello world", "hello_world"),
+            ("hello-world test", "hello--world_test"),
+        ]:
+
+            self.assertEqual(Format.badge(x), expected_result)
+
+    def test_and_list(self):
+        for x, expected_result in [
+            ([], ""),
+            (["apple"], "apple"),
+            (["apple", "banana"], "apple & banana"),
+            (["apple", "banana", "cherry"], "apple, banana & cherry"),
+        ]:
+
+            self.assertEqual(Format.and_list(x), expected_result)
+
+    def test_title(self):
+        for x, expected_result in [
+            ("hello_world", "Hello World"),
+            ("Lk_news", "🇱🇰 #SriLanka News"),
+            ("test_Lk_case", "Test 🇱🇰 #SriLanka Case"),
+        ]:
+
+            self.assertEqual(Format.title(x), expected_result)
