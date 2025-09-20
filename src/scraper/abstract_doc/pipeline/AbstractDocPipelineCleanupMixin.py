bd521785bff7e9ea109cfc9a024e8d2aca95afe8
nuuuwan <nuuuwan@gmail.com>
2025-09-20 10:08:22 +0530

    remove legacy_chart_docs_by_year_path

--- src/scraper/abstract_doc/pipeline/AbstractDocPipelineCleanupMixin.py
+++ src/scraper/abstract_doc/pipeline/AbstractDocPipelineCleanupMixin.py
@@ -16,3 +16,10 @@ class AbstractDocPipelineCleanupMixin:
         if os.path.exists(legacy_dir_hugging_face_data):
             shutil.rmtree(legacy_dir_hugging_face_data)
             log.warning(f"🧹 Deleted {legacy_dir_hugging_face_data}")
+
+        legacy_chart_docs_by_year_path = os.path.join(
+            cls.get_dir_docs_for_cls(), "docs_by_year.png"
+        )
+        if os.path.exists(legacy_chart_docs_by_year_path):
+            os.remove(legacy_chart_docs_by_year_path)
+            log.warning(f"🧹 Deleted {legacy_chart_docs_by_year_path}")
