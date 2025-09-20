5f61551d724532f36b06bed67f6bdb898b30162d
nuuuwan <nuuuwan@gmail.com>
2025-09-20 07:43:44 +0530

    Made the push parallel

--- push_to_clients.sh
+++ push_to_clients.sh
@@ -10,42 +10,45 @@ CLIENT_REPOS=(
     "lk_legal_docs"    
 )
 
-
-
-for CLIENT_REPO in "${CLIENT_REPOS[@]}"; do
-    echo "------------------------------------------------------"
-    echo "Pushing to $CLIENT_REPO..."
-    echo "------------------------------------------------------"
-
-    DIR_CLIENT_REPO="../$CLIENT_REPO"
-
-    [[ -d "$DIR_CLIENT_REPO" ]] || { echo "❌ Missing source dir: $DIR_CLIENT_REPO"; exit 1; }
+function push_to_client_repo() {
+    local CLIENT_REPO="$1"
     
-    cd $DIR_CLIENT_REPO;
+    function inner() {
+        DIR_CLIENT_REPO="../$CLIENT_REPO"
 
-    git reset --hard HEAD;
-    git clean -fd;
+        [[ -d "$DIR_CLIENT_REPO" ]] || { echo "❌ Missing source dir: $DIR_CLIENT_REPO"; exit 1; }
+        
+        cd "$DIR_CLIENT_REPO" || exit 1
 
-    git pull origin main --rebase;
+        git reset --hard HEAD
+        git clean -fd
 
-    git rm -rf src/pdf_scraper
-    git commit -m "[push_to_clients] Deleted pdf_scraper"
+        git pull origin main --rebase
 
-    rm -rf src/scraper
-    cp -r ../scraper/src/scraper src/
-    git add src/scraper
-    git commit -m "[push_to_clients] Updated scraper"
+        rm -rf src/scraper
+        cp -r ../scraper/src/scraper src/
+        git add src/scraper
+        git commit -m "[push_to_clients] Updated scraper"
 
-    rm -rf src/utils_future
-    cp -r ../scraper/src/utils_future src/
-    git add src/utils_future
-    git commit -m "[push_to_clients] Updated utils_future"
+        rm -rf src/utils_future
+        cp -r ../scraper/src/utils_future src/
+        git add src/utils_future
+        git commit -m "[push_to_clients] Updated utils_future"
 
-    git push origin main
-    # open https://github.com/nuuuwan/$CLIENT_REPO/actions/workflows/pipeline.yml
-    # code $DIR_CLIENT_REPO
+        git push origin main
+
+        # open https://github.com/nuuuwan/$CLIENT_REPO/actions/workflows/pipeline.yml
+        # code $DIR_CLIENT_REPO
+    }
+    inner > /dev/null 2>&1 
 
     echo "✅ Push to $CLIENT_REPO complete."
+}
+
+for CLIENT_REPO in "${CLIENT_REPOS[@]}"; do
+    push_to_client_repo "$CLIENT_REPO" &
 done
 
 
+wait
+echo "✅✅ ALL pushes complete."
