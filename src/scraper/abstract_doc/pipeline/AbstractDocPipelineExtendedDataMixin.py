5c9ab4a23835941538fcf2a9ab549c182a62f67d
nuuuwan <nuuuwan@gmail.com>
2025-09-21 10:05:02 +0530

    Added doc_batch concept. But processing is still serial

--- src/scraper/abstract_doc/pipeline/AbstractDocPipelineExtendedDataMixin.py
+++ src/scraper/abstract_doc/pipeline/AbstractDocPipelineExtendedDataMixin.py
@@ -1,5 +1,6 @@
 import os
 import time
+from typing import Generator
 
 from utils import Log
 
@@ -7,15 +8,31 @@ log = Log("AbstractDocPipelineExtendedDataMixin")
 
 
 class AbstractDocPipelineExtendedDataMixin:
+    EXTENDED_PROCESSING_BATCHS_SIZE = 8
+
     @classmethod
-    def scrape_all_extended_data(cls, max_dt):
-        t_start = time.time()
-        for doc in cls.list_all():
+    def gen_doc_batch_list(cls) -> Generator[list, None, None]:
+        all_docs = cls.list_all()
+        for i in range(0, len(all_docs), cls.EXTENDED_PROCESSING_BATCHS_SIZE):
+            i_start = i
+            i_end = min(
+                i + cls.EXTENDED_PROCESSING_BATCHS_SIZE, len(all_docs)
+            )
+            yield all_docs[i_start:i_end]
+
+    @classmethod
+    def process_doc_batch(cls, doc_batch):
+        for doc in doc_batch:
             try:
                 doc.scrape_extended_data_for_doc()
             except Exception as e:
                 log.error(f"Error scraping extended data for {doc}: {e}")
 
+    @classmethod
+    def scrape_all_extended_data(cls, max_dt):
+        t_start = time.time()
+        for doc_batch in cls.gen_doc_batch_list():
+            cls.process_doc_batch(doc_batch)
             dt = time.time() - t_start
             if dt > max_dt:
                 log.info(f"🛑 Stopping. {dt:,.1f}s > {max_dt:,}s")
