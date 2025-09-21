68cf6c373379df9848cb173e34d08d4242b35dfd
nuuuwan <nuuuwan@gmail.com>
2025-09-21 10:14:35 +0530

    added logging

--- src/scraper/abstract_doc/pipeline/AbstractDocPipelineExtendedDataMixin.py
+++ src/scraper/abstract_doc/pipeline/AbstractDocPipelineExtendedDataMixin.py
@@ -8,15 +8,15 @@ log = Log("AbstractDocPipelineExtendedDataMixin")
 
 
 class AbstractDocPipelineExtendedDataMixin:
-    EXTENDED_PROCESSING_BATCHS_SIZE = 4
+    BATCH_SIZE = 4
     MAX_THREADS = 4
 
     @classmethod
     def gen_doc_batch_list(cls) -> Generator[list, None, None]:
         all_docs = cls.list_all()
-        for i in range(0, len(all_docs), cls.EXTENDED_PROCESSING_BATCHS_SIZE):
+        for i in range(0, len(all_docs), cls.BATCH_SIZE):
             i_start = i
-            i_end = min(i + cls.EXTENDED_PROCESSING_BATCHS_SIZE, len(all_docs))
+            i_end = min(i + cls.BATCH_SIZE, len(all_docs))
             yield all_docs[i_start:i_end]
 
     @classmethod
@@ -29,6 +29,9 @@ class AbstractDocPipelineExtendedDataMixin:
 
     @classmethod
     def scrape_all_extended_data(cls, max_dt):
+        log.debug(f"BATCH_SIZE={cls.BATCH_SIZE}")
+        log.debug(f"MAX_THREADS={cls.MAX_THREADS}")
+
         t_start = time.time()
         for doc_batch in cls.gen_doc_batch_list():
             cls.process_doc_batch(doc_batch)
