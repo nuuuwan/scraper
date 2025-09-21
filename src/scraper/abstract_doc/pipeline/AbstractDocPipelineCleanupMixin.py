fd00a85e860a0ef84fd1b7f5d7de08e544abc75f
nuuuwan <nuuuwan@gmail.com>
2025-09-21 08:15:44 +0530

    cleanup bug

--- src/scraper/abstract_doc/pipeline/AbstractDocPipelineCleanupMixin.py
+++ src/scraper/abstract_doc/pipeline/AbstractDocPipelineCleanupMixin.py
@@ -20,8 +20,11 @@ class AbstractDocPipelineCleanupMixin:
             log.warning(f"🧹 Deleted {legacy_dir_hugging_face_data}")
 
         # cleanup cls.get_dir_docs_for_cls()
-        for file_name in os.listdir(cls.get_dir_docs_for_cls()):
-            if file_name.endswith(".png"):
-                file_path = os.path.join(cls.get_dir_docs_for_cls(), file_name)
-                os.remove(file_path)
-                log.warning(f"🧹 Deleted {file_path}")
+        if os.path.exists(cls.get_dir_docs_for_cls()):
+            for file_name in os.listdir(cls.get_dir_docs_for_cls()):
+                if file_name == "docs_by_year_and_lang.png":
+                    file_path = os.path.join(
+                        cls.get_dir_docs_for_cls(), file_name
+                    )
+                    os.remove(file_path)
+                    log.warning(f"🧹 Deleted {file_path}")
