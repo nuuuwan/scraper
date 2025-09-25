8dcde62fe431b7a60cba0b76fb78d7023017a395
nuuuwan <nuuuwan@gmail.com>
2025-09-25 17:51:47 +0530

    Added compress

--- src/utils_future/pdf_file/PDFFile.py
+++ src/utils_future/pdf_file/PDFFile.py
@@ -19,3 +19,18 @@ class PDFFile(FileOrDirFuture, PDFTextMixin):
         pix = page.get_pixmap()
         pix.save(image_path)
         log.info(f"Wrote image {self} to {image_path}")
+
+    def compress(self, compressed_pdf_path: str) -> None:
+        doc = pymupdf.open(self.path)
+        doc.save(
+            compressed_pdf_path,
+            garbage=4,  # remove redundant objects
+            deflate=True,  # apply zlib compression to streams
+            clean=True,  # clean up unused resources
+        )
+        compressed_pdf_file = PDFFile(compressed_pdf_path)
+        p_compress = 100 * (1 - compressed_pdf_file.size / self.size)
+        log.info(
+            f"Compressed {self} -> {compressed_pdf_file} ({p_compress:.1f}%)"
+        )
+        return compressed_pdf_file
