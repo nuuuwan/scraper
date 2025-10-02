2b48d685d3c4bab645fd9535572e3a887cf22591
nuuuwan <nuuuwan@gmail.com>
2025-10-02 17:00:15 +0530

    BUGFIX AbstractDoc module import

--- src/scraper/abstract_doc/core/AbstractDocGeneratorMixin.py
+++ src/scraper/abstract_doc/core/AbstractDocGeneratorMixin.py
@@ -1,10 +1,7 @@
 from abc import ABC
-from typing import Generator
-
-from scraper.abstract_doc import AbstractDoc
 
 
 class AbstractDocGeneratorMixin(ABC):
     @classmethod
-    def gen_docs(cls) -> Generator[AbstractDoc, None, None]:
+    def gen_docs(cls):
         pass
