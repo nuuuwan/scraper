6fa2ce31d1eaafa159ef602478e7aa0f9dc69047
nuuuwan <nuuuwan@gmail.com>
2025-10-14 10:01:50 +0530

    Added tests for AbstractExcelSpreadsheet

---
+++ tests/scraper/test_abstract_excel_spreadsheet.py
@@ -0,0 +1,24 @@
+import os
+import unittest
+
+from scraper import AbstractExcelSpreadsheet
+
+
+class TestCase(unittest.TestCase):
+    def test_init(self):
+        doc = AbstractExcelSpreadsheet(
+            num="1234567890",
+            date_str="2023-10-01",
+            description="Test Document",
+            url_metadata="http://mock.com/doc.html",
+            lang="en",
+            url_excel="http://mock.com/doc.xlsx",
+        )
+        self.assertEqual(doc.num, "1234567890")
+        self.assertEqual(doc.url_excel, "http://mock.com/doc.xlsx")
+        self.assertFalse(doc.has_excel)
+        with self.assertRaises(FileNotFoundError):
+            doc.download_excel()
+        self.assertEqual(
+            doc.excel_path, os.path.join(doc.dir_doc, "doc.xlsx")
+        )
