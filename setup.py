593dc55f5d13edde91018a9dd5fb2958549fa8f9
nuuuwan <nuuuwan@gmail.com>
2025-10-14 09:56:17 +0530

    VERSION = "1.0.5"

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 import setuptools
 
 DIST_NAME = "scraper"
-VERSION = "1.0.4"
+VERSION = "1.0.5"
 DESCRIPTION = "Utilities for Scraping Documents from Websites."
 INSTALL_REQUIRES = [
     "utils-nuuuwan",
