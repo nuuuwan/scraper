8e3049979caa375613ceb929d15a844f04c41c7c
nuuuwan <nuuuwan@gmail.com>
2025-10-15 09:16:39 +0530

    Added TestExcelSpreadsheet

--- tests/scraper/test_abstract_excel_spreadsheet.py
+++ tests/scraper/test_abstract_excel_spreadsheet.py
@@ -3,9 +3,11 @@ import unittest
 from scraper import AbstractExcelSpreadsheet
 
 
-class TestCase(unittest.TestCase):
-    def test_init(self):
-        doc = AbstractExcelSpreadsheet(
+class TestExcelSpreadsheet(AbstractExcelSpreadsheet):
+
+    @classmethod
+    def gen_docs(cls):
+        yield TestExcelSpreadsheet(
             num="1234567890",
             date_str="2023-10-01",
             description="Test Document",
@@ -13,5 +15,10 @@ class TestCase(unittest.TestCase):
             lang="en",
             url_excel="http://mock.com/doc.xlsx",
         )
+
+
+class TestCase(unittest.TestCase):
+    def test_init(self):
+        doc = TestExcelSpreadsheet.gen_docs().__next__()
         self.assertEqual(doc.num, "1234567890")
         self.assertEqual(doc.url_excel, "http://mock.com/doc.xlsx")
